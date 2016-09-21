2926ce242b655005f5c1d4dd80426eddb42e7c3c
sal <slpg500a>
2016-09-21 23:31:58 +0100

    Variable port

--- src/capture0/main.py
+++ src/capture0/main.py
@@ -1,11 +1,11 @@
-import copy
-import os
-import logging
 import click
+import copy
 import json
+import logging
+import os
 import uuid
-from typing import Mapping
 from flask import Flask, request, make_response
+from typing import Mapping
 
 log = logging.getLogger(__name__)
 
@@ -13,12 +13,13 @@ app = Flask(__name__)
 
 SAVE_DIR = os.path.join(os.path.expanduser("~"), "capture0")
 
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 
-def store_data(dataset:str, record:Mapping):
+def store_data(dataset: str, record: Mapping):
     record = copy.copy(record)
     id = str(uuid.uuid1())
     dataset_dir = os.path.join(SAVE_DIR, dataset)
@@ -34,24 +35,25 @@ def store_data(dataset:str, record:Mapping):
         log.info("Saved %s" % file_path)
 
 
-
 @app.route('/save/<string:dataset>', methods=["POST"])
 def save(dataset):
     log.info(request.content_type)
     log.info(request.data)
     data = json.loads(request.data.decode("UTF-8"))
 
-
     store_data(dataset, data)
 
     return make_response("OK")
 
+
 @click.command()
 @click.option("--port", default=59893, help="Which port should the server run on?")
-def main(port):
+@click.option("--host", default="0.0.0.0", help="Which interface should the server run on?")
+def main(host, port):
     logging.basicConfig()
     logging.getLogger("").setLevel(logging.INFO)
-    app.run(port=port)
+    app.run(host=host, port=port)
+
 
 if __name__ == '__main__':
     main()
