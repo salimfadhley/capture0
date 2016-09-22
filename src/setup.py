3e02a3ea3a86c48aa9aef1eed00e72ca1665a26c
sal <slpg500a>
2016-09-22 21:23:44 +0100

    Bump version: 0.0.12 → 0.1.0

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-__VERSION__ = '0.0.12'
+__VERSION__ = '0.1.0'
 PROJECT_AUTHORS = "Salim Fadhley"
 PROJECT_EMAILS = 'salimfadhley@gmail.com'
 PROJECT_URL = "https://github.com/salimfadhley/capture0"
