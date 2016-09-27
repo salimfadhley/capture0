03cadcc79d057c470963fa1a9a531a18770cdf4b
sal <slpg500a>
2016-09-28 00:40:25 +0100

    bump

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-__VERSION__ = '0.1.34'
+__VERSION__ = '0.1.36'
 PROJECT_AUTHORS = "Salim Fadhley"
 PROJECT_EMAILS = 'salimfadhley@gmail.com'
 PROJECT_URL = "https://github.com/salimfadhley/capture0"
