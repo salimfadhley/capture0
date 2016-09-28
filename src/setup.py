9a53b7b8256dbe6b0bb028f337b4f14fd822f6cf
sal <slpg500a>
2016-09-28 01:05:57 +0100

    add some images

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-__VERSION__ = '0.1.39'
+__VERSION__ = '0.1.40'
 PROJECT_AUTHORS = "Salim Fadhley"
 PROJECT_EMAILS = 'salimfadhley@gmail.com'
 PROJECT_URL = "https://github.com/salimfadhley/capture0"
